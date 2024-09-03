5e700cbe9d3fa9debfaa370c5c24dba86b9634e4
Axel <axel@Axel-Hernandez-L77X5JMQ7R.local>
2024-09-03 16:50:10 -0600

    adding go to store bunctionality to the game

--- script.js
+++ script.js
@@ -1,19 +1,60 @@
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
 const button3 = document.getElementById('button3');
-const textContent = document.getElementById('textContent');
+const text = document.getElementById('text');
+//stats text content
+const userXp = document.getElementById('xp');
+const userHealth = document.getElementById('health');
+const userGold = document.getElementById('gold');
+
+//user stats variables
+let xp = 0;
+let health = 100;
+let gold = 50;
+
 console.log(button1);
 
+//Creating the locations
+const locations = [
+    {
+        name: 'store',
+        'buttons text': ['Buy 10 Health (10 gold)', 'Buy weapon (30 gold)', 'Go to town square'],
+        'button functions': [buyHealth, buyWeapon, goTown]
+    }
+];
+//update function (updates the text 'context' environment, base on location)
+function update(locations){
+    if(locations[0]){
+        button1.textContent = locations[0]['buttons text'][0];
+        button2.textContent = locations[0]['buttons text'][1];
+        button3.textContent = locations[0]['buttons text'][2];
+        text.textContent = 'You enter the store';
+    }
+}
 
-// button1.addEventlistener('click', function(){
-//     button1.innerText = "Buy 10 health (10 gold)";
+//Functions for store
+function buyHealth(){
+    if(gold >= 10){
+        health += 10;
+        gold -= 10;
+        userHealth.textContent = health;
+        userGold.textContent = gold;
+    } else {
+        text.textContent = 'You have not enough gold to buy more health';
+    }
+}
 
-//     textContent.innerText = "You enter the store.";
-// })
+function buyWeapon(){
+
+}
+
+
+function goTown(){
+
+}
+
+//Store event listener
 button1.addEventListener('click', function(){
-    button1.innerText = "Buy 10 Health (10 gold)";
-    button2.innerText = "Buy weapon (30 gold)";
-    button3.innerText = "Go to town square";
-
-    textContent.innerText = "You enter the store";
+    update(locations);
+    button1.onclick = buyHealth;
 })
